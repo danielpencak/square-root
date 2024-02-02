5ba8fb982479b311b1a31193149c2dc5487b7429
Daniel Pencak <danielpencak@gmail.com>
2024-02-02 09:40:42 -0600

    Setup functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { SyntheticEvent, useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState<number | null>(null);
+  const [numberToTakeSquareRootOf, setNumberToTakeSquareRootOf] = useState<
+    number | null
+  >(null);
+  const [precision, setPrecision] = useState<number | null>(null);
+
+  const handleNumberToFindSquareRootOfChange = (e: SyntheticEvent): void => {
+    const target = e.target as HTMLInputElement;
+    setNumberToTakeSquareRootOf(Number.parseFloat(target.value));
+  };
+
+  const handlePrecisionChange = (e: SyntheticEvent): void => {
+    const target = e.target as HTMLInputElement;
+    setPrecision(Number.parseFloat(target.value));
+  };
+
+  // Reference: https://stackoverflow.com/questions/52468506/how-to-calculate-the-square-root-without-using-library-and-built-in-methods-in-j
+  const calculateSquareRoot = (): void => {
+    if (precision !== null && precision <= 0) {
+      alert('Please enter a positive integer for the precision input.');
+      return;
+    }
+
+    if (numberToTakeSquareRootOf !== null && numberToTakeSquareRootOf < 0) {
+      alert('Please enter a positive number to take the square root of.');
+      return;
+    }
+
+    if (numberToTakeSquareRootOf === 0 || numberToTakeSquareRootOf === 1) {
+      setResult(numberToTakeSquareRootOf);
+      return;
+    }
+
+    let x;
+    let x1 = (numberToTakeSquareRootOf as number) / 2;
+
+    do {
+      x = x1;
+      x1 = (x + (numberToTakeSquareRootOf as number) / x) / 2;
+    } while (x !== x1);
+
+    setResult(Number.parseFloat(x.toFixed(precision as number)));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Find the square root!</h1>
       </header>
+      <main>
+        <label htmlFor="number-to-find-square-root-of">
+          Enter the number you would like to find the square root of.
+        </label>
+        <input
+          id="number-to-find-square-root-of"
+          name="number-to-find-square-root-of"
+          placeholder="Enter number..."
+          type="number"
+          min="0"
+          onChange={handleNumberToFindSquareRootOfChange}
+        />
+        <label htmlFor="precision">
+          Enter an integer representing the precision. (Ex. 4 = .XXXX)
+        </label>
+        <input
+          id="precision"
+          name="precision"
+          placeholder="Enter precision..."
+          type="number"
+          min="1"
+          onChange={handlePrecisionChange}
+        />
+        <button
+          disabled={precision === null || numberToTakeSquareRootOf === null}
+          onClick={calculateSquareRoot}
+        >
+          Calculate!
+        </button>
+        {(!!result || result === 0) && <h2>Result: {result}</h2>}
+      </main>
     </div>
   );
 }
